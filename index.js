9ac2b61dad1b390337063852cc1852e3299892b5
kausalya-21MID0154 <kausalya.2021@vitstudent.ac.in>
2024-07-07 23:34:50 +0530

    commit

--- index.js
+++ index.js
@@ -26,3 +26,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Hello from the root route!");
+});
